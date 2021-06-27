75b72c30873dd3afcf572c316163d555dc6981b3
Hani Hussein <hani.hussein94@gmail.com>
2021-06-27 12:19:02 +0200

    fix login-screen mobile design

--- lib/ui/screens/login/widgets/rightSide.dart
+++ lib/ui/screens/login/widgets/rightSide.dart
@@ -1,3 +1,4 @@
+import 'package:admin/responsive.dart';
 import 'package:admin/shared/constants.dart';
 import 'package:admin/ui/screens/main/main_screen.dart';
 import 'package:admin/services/login_service.dart';
@@ -49,143 +50,147 @@ class _RightSideState extends State<RightSide> {
   @override
   Widget build(BuildContext context) {
     return Flexible(
-      flex: 3,
+      flex: Responsive.isMobile(context) ? 4 : 3,
       child: Container(
         padding: EdgeInsets.all(30),
         color: Colors.white,
         child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  // Text(
-                  //   'ElShawadfy Radiology System',
-                  //   style: TextStyle(
-                  //       fontWeight: FontWeight.bold,
-                  //       fontSize: 25,
-                  //       color: Colors.pink,
-                  //       letterSpacing: 2),
-                  // ),
-                  // Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-                  Text(
-                    'Sign in',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: MediaQuery.of(context).size.width * 0.03,
-                        color: Colors.black),
-                  ),
-                ],
-              ),
-              // Container(
-              //     margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-              //     child: Text(
-              //       'Enter your details below',
-              //       style: TextStyle(color: Colors.grey),
-              //     )),
-
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: TextField(
-                  controller: usernameController,
-                  onSubmitted: (value) {
-                    login();
-                  },
-                  style: TextStyle(color: primaryColor),
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    icon: Image.asset('assets/images/user.png'),
-                    labelStyle: TextStyle(color: secondaryColor),
-
-                    // prefix: Image.asset('images/user.png'),
-                    // prefixIcon: Icon(Icons.person),
-                    border: new OutlineInputBorder(
-                        borderSide: new BorderSide(color: Colors.grey)),
-                    enabledBorder: new OutlineInputBorder(
-                        borderSide: new BorderSide(color: Colors.grey)),
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  onSubmitted: (value) {
-                    login();
-                  },
-                  style: TextStyle(color: primaryColor),
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: secondaryColor),
-                    icon: Image.asset('assets/images/closed-lock.png'),
-                    // prefix: Image.asset('images/closed-lock.png'),
-                    // prefixIcon: Icon(Icons.person),
-                    border: new OutlineInputBorder(
-                        borderSide: new BorderSide(color: Colors.grey)),
-                    enabledBorder: new OutlineInputBorder(
-                        borderSide: new BorderSide(color: Colors.grey)),
-                  ),
-                ),
-              ),
-              errorText == null
-                  ? Container()
-                  : Container(
-                      margin: EdgeInsets.only(top: 5),
-                      alignment: Alignment.center,
-                      child: Text(
-                        '$errorText',
-                        style: TextStyle(
-                            color: Colors.red,
-                            fontSize: MediaQuery.of(context).size.width * 0.02),
-                      ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // Text(
+                    //   'ElShawadfy Radiology System',
+                    //   style: TextStyle(
+                    //       fontWeight: FontWeight.bold,
+                    //       fontSize: 25,
+                    //       color: Colors.pink,
+                    //       letterSpacing: 2),
+                    // ),
+                    // Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                    Text(
+                      'Sign in',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.width * 0.03,
+                          color: Colors.black),
                     ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: isLoading
-                    ? CircularProgressIndicator()
-                    : ElevatedButton(
-                        onPressed: () => login(),
+                  ],
+                ),
+                // Container(
+                //     margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                //     child: Text(
+                //       'Enter your details below',
+                //       style: TextStyle(color: Colors.grey),
+                //     )),
+
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: TextField(
+                    controller: usernameController,
+                    onSubmitted: (value) {
+                      login();
+                    },
+                    style: TextStyle(color: primaryColor),
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      icon: Image.asset('assets/images/user.png'),
+                      labelStyle: TextStyle(color: secondaryColor),
+
+                      // prefix: Image.asset('images/user.png'),
+                      // prefixIcon: Icon(Icons.person),
+                      border: new OutlineInputBorder(
+                          borderSide: new BorderSide(color: Colors.grey)),
+                      enabledBorder: new OutlineInputBorder(
+                          borderSide: new BorderSide(color: Colors.grey)),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    onSubmitted: (value) {
+                      login();
+                    },
+                    style: TextStyle(color: primaryColor),
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: secondaryColor),
+                      icon: Image.asset('assets/images/closed-lock.png'),
+                      // prefix: Image.asset('images/closed-lock.png'),
+                      // prefixIcon: Icon(Icons.person),
+                      border: new OutlineInputBorder(
+                          borderSide: new BorderSide(color: Colors.grey)),
+                      enabledBorder: new OutlineInputBorder(
+                          borderSide: new BorderSide(color: Colors.grey)),
+                    ),
+                  ),
+                ),
+                errorText == null
+                    ? Container()
+                    : Container(
+                        margin: EdgeInsets.only(top: 5),
+                        alignment: Alignment.center,
                         child: Text(
-                          'Login',
+                          '$errorText',
                           style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
+                              color: Colors.red,
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.02),
                         ),
-                        style: ElevatedButton.styleFrom(
-                          primary: primaryColor,
-                        )),
-              ),
-              // ),
-              // Container(
-              //     margin: EdgeInsets.only(top: 30),
-              //     padding: EdgeInsets.only(top: 20),
-              //     width: 300,
-              //     decoration: BoxDecoration(
-              //         border: Border(
-              //             top: BorderSide(
-              //       color: Colors.grey,
-              //     ))),
-              //     child: Row(
-              //       mainAxisAlignment: MainAxisAlignment.center,
-              //       children: <Widget>[
-              //         Image.asset('assets/images/facebook.png'),
-              //         SizedBox(
-              //           width: 20,
-              //         ),
-              //         Image.asset('assets/images/youtube.png'),
-              //         SizedBox(
-              //           width: 20,
-              //         ),
-              //         Image.asset('assets/images/twitter.png'),
-              //         SizedBox(
-              //           width: 20,
-              //         ),
-              //         Image.asset('assets/images/github.png'),
-              //       ],
-              //     )),
-            ],
+                      ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: isLoading
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                          onPressed: () => login(),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: primaryColor,
+                          )),
+                ),
+                // ),
+                // Container(
+                //     margin: EdgeInsets.only(top: 30),
+                //     padding: EdgeInsets.only(top: 20),
+                //     width: 300,
+                //     decoration: BoxDecoration(
+                //         border: Border(
+                //             top: BorderSide(
+                //       color: Colors.grey,
+                //     ))),
+                //     child: Row(
+                //       mainAxisAlignment: MainAxisAlignment.center,
+                //       children: <Widget>[
+                //         Image.asset('assets/images/facebook.png'),
+                //         SizedBox(
+                //           width: 20,
+                //         ),
+                //         Image.asset('assets/images/youtube.png'),
+                //         SizedBox(
+                //           width: 20,
+                //         ),
+                //         Image.asset('assets/images/twitter.png'),
+                //         SizedBox(
+                //           width: 20,
+                //         ),
+                //         Image.asset('assets/images/github.png'),
+                //       ],
+                //     )),
+              ],
+            ),
           ),
         ),
       ),
